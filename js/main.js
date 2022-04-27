8cbdcd66559df1ca51e1fb4db5f9a4f8a7562050
Gleb <iamgleb.g@gmail.com>
2022-04-27 05:52:45 +0300

    1.3.4

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 let switcher = UIkit.switcher('.uk-switcher');
+
+let ContactBody = document.querySelector('.contact__body');
+let form = document.querySelector('.contact__form');
+
+form.onsubmit=function(event) { 
+  event.preventDefault();
+    ContactBody.classList.add("animation--fly-out");
+    setTimeout(function(){
+      document.querySelector('.animation--fly-in').style.display = "flex";
+     },3000);
+  setTimeout(function(){
+    form.requestSubmit();
+     },2000);     
+};
