1db9ef4142f451c79956d3340e030f0fee3cd2b0
Gleb <iamgleb.g@gmail.com>
2022-04-28 23:54:39 +0300

    1.3.9

--- js/main.js
+++ js/main.js
@@ -3,13 +3,50 @@
 let ContactBody = document.querySelector('.contact__body');
 let form = document.querySelector('.contact__form');
 
-form.onsubmit=function(event) { 
-  event.preventDefault();
+const validation = new JustValidate('#form', {
+  errorFieldCssClass: 'error',
+  focusInvalidField: false,
+  lockForm: true,
+});
+
+validation
+  .addField('#name', [
+    {
+      rule: 'required',
+      errorMessage: ' ',
+    },
+  ])
+  .addField('#social', [
+    {
+      rule: 'required',
+      errorMessage: ' ',
+    },
+  ])
+  .addField('#message', [
+    {
+      rule: 'required',
+      errorMessage: ' ',
+    },
+  ])
+  
+  .onSuccess((event) => {
+    console.log('Validation passes and form submitted');
     ContactBody.classList.add("animation--fly-out");
     setTimeout(function(){
       document.querySelector('.animation--fly-in').style.display = "flex";
      },3000);
-  setTimeout(function(){
+    setTimeout(function(){
     form.requestSubmit();
-     },2000);     
-};
+     },2000);
+  });
+
+// form.onsubmit=function(event) { 
+//   event.preventDefault();
+//     ContactBody.classList.add("animation--fly-out");
+//     setTimeout(function(){
+//       document.querySelector('.animation--fly-in').style.display = "flex";
+//      },3000);
+//     setTimeout(function(){
+//     form.requestSubmit();
+//      },2000);     
+// };
